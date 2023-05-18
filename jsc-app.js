ef981b2f80be3b9674e104407740b24f85df5be0
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-05-18 16:28:04 +0900

    서버 만들어서 NAR 가운데 띄우기

--- jsc-app.js
+++ jsc-app.js
@@ -1,6 +1,6 @@
-import url from "url";
-import http from "http";
-import fs from "fs";
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const parseUrl = url.parse(req.url);
@@ -10,6 +10,29 @@ const server = http.createServer((req, res) => {
   if (method === "GET" && pathName === "/") {
     fs.readFile("./test.html", () => {
       res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Document</title>
+          <style>
+            #root {
+              width : 100vw;
+              height : 100vh;
+              display: flex;
+              flex-direction: row;
+              justify-content: center;
+              align-items: center;
+            }
+          </style>
+        </head>
+        <body>
+          <div id="root">NAR</div>
+          <script></script>
+        </body>
+      </html>`);
       res.end();
     });
   }

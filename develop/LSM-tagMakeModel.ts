a624eb45acd5c89f31f8403158447dd77850c480
leesemin <leesemin222@gmail.com>
2023-05-19 16:41:54 +0900

    태그매이커로 사용해서 만들기

--- develop/LSM-tagMakeModel.ts
+++ develop/LSM-tagMakeModel.ts
@@ -95,3 +95,51 @@ tagMake2.map((element): void => {
 const mainDiv = tagMake(tagMake1[0], tagMake1[1], tagMake1[2]);
 
 module.exports = mainDiv;
+function tagMake(tagName: string = "div", innerHTML: string = "", tagMore: string = ""): string {
+  return `<${tagName} ${tagMore}> ${innerHTML} </${tagName}>`;
+}
+
+function tagStyle(object: { [key: string]: string | number }): string {
+  let style = 'style="';
+  for (let key in object) {
+    style += `${key}:${object[key]};`;
+  }
+  style += '"';
+  return style;
+}
+
+const divStyle: {} = {
+  width: "100vw",
+  height: "100vh",
+  display: "flex",
+  "align-items": "center",
+  "justify-content": "center",
+};
+
+const tagList: {} = {
+  make1: {
+    tagName: "div",
+    innerHTML: "",
+    tagMore: "id='DB-view'",
+  },
+  make2: {
+    tagName: "",
+    innerHTML: "",
+    tagMore: "",
+  },
+  make3: {
+    tagName: "",
+    innerHTML: "",
+    tagMore: "",
+  },
+  make4: {
+    tagName: "",
+    innerHTML: "",
+    tagMore: "",
+  },
+};
+
+const mainDiv = tagMake("div", "NaR", tagStyle(divStyle));
+console.log(mainDiv);
+
+module.exports = mainDiv;

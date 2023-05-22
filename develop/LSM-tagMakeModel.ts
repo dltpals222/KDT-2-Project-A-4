427cff8aa65d62abbe21fd86201e8c3bdc56ce8d
leesemin <leesemin222@gmail.com>
2023-05-22 14:14:16 +0900

    날린거 복구 중

--- develop/LSM-tagMakeModel.ts
+++ develop/LSM-tagMakeModel.ts
@@ -1,4 +1,5 @@
-import fs from 'fs'
+import * as fs from 'fs'
+import * as path from 'path'
 
 function tagMake(tagName: string = "div", innerHTML: string = "", tagMore: string = ""): string{
   return `<${tagName} ${tagMore}>${innerHTML}</${tagName}>`
@@ -39,33 +40,17 @@ interface companyParseInterface {
 const company = fs.readFileSync('../financeDB_testData(samsung).json','utf-8')
 const companyParse:companyParseInterface = JSON.parse(company)
 const companyInner:companyInterface[] = companyParse.samsung
-
-console.log(companyInner)
-
-class CompanyInnerHTML {
-  private company : companyInterface[];
-
-  constructor(company : companyInterface[]){
-    this.company = company
-  }
-
-
-  get parseData(){
-    let value:string = "";
-    for(let i in this.company){
-      value += 
-    }
-  }
-}
-
-  
-}
-
+const companyNo = companyInner.map((value) => {value.no});
+const companyOpen = companyInner.map((value) => {value.open});
+const companyHigh = companyInner.map((value) => {value.high});
+const companyLow = companyInner.map((value) => {value.low});
+const companyClose = companyInner.map((value) => {value.close});
+const companyVolume = companyInner.map((value) => {value.volume});
+const companyDay  = companyInner.map((value) => {value.day});
 
 
 const tagMake1 : string[] = ['div','',tagStyle(divStyle) + 'id=\"DB-view\"']; //root의 자식
 const tagMake2 : string[][] = [['div',"",'id=\"no\"'],['div','','id=\"open\"'],['div','','id=\"high\"'],['div','','id=\"low\"'],['div','','id=\"close\"'],['div','','id=\"volume\"'],['div','','id=\"day\"']]; //7개
-const tagMake3 : string[][] = [['div','',''],['ul','','']]; //2개
 
 
 let DBColumn : string="";
@@ -74,9 +59,6 @@ let fourList : string = "";
 
 
 
-tagMake3.map((Element) : void=>{
-  twoChild += tagMake(Element[0])
-})
 
 tagMake2.map((element) : void=>{
   DBColumn += tagMake(element[0],twoChild,element[2])

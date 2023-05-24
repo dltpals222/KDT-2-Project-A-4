fc4f486c9ba25d9612ba418d224bea30dbca4044
leesemin <leesemin222@gmail.com>
2023-05-24 09:51:17 +0900

    HTML에 데이터 띄우기 성공

--- develop/LSM-tagMakeModel.ts
+++ develop/LSM-tagMakeModel.ts
@@ -92,13 +92,18 @@ const companyInfoAll = (company: CompanyArrayType, count: number = -1): string =
 const wantCompanyInfo = (company: CompanyArrayType, count: number = -1): string => {
   let twoChild: string = "";
   tagMake3Text.map((element) => {
-    twoChild += tagMake("div", element);
-    twoChild += tagMake("ul", companyInfoAll(company, count));
+    twoChild += tagMake(
+      "div",
+      tagMake("div", element) +
+        tagMake("ul", companyInfoAll(company, count), tagStyle({ "list-style-type": "none", padding: 0, margin: 0 })),
+      tagStyle({ border: "1px solid black", padding: "1.5%" })
+    );
+    // twoChild += tagMake("div", element);
+    // twoChild += tagMake("ul", companyInfoAll(company, count));
   });
   return twoChild;
 };
 
 const mainDiv = tagMake(tagMake1[0], wantCompanyInfo(tagMake3CompanyData[0], 10), tagMake1[2]);
-console.log(mainDiv);
 
 module.exports = mainDiv;

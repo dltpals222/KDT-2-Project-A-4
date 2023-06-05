28eaa2a98c97d7fe3c94e42d38bdefd1f521c846
leesemin <leesemin222@gmail.com>
2023-06-05 17:03:21 +0900

    저장

--- develop/login/login.tsx
+++ develop/login/login.tsx
@@ -19,7 +19,7 @@ function login() {
           <div>주식 추천 앱</div>
           <div>운운운운운운</div>
         </div>
-        <form action="/" method="GET">
+        <form action="/main" method="POST">
           <input style={inputTextStyle} type="text" placeholder="아이디"></input>
           <input style={inputTextStyle} type="text" placeholder="패스워드"></input>
           <input style={inputSubmitStyle} type="submit" value="로그인"></input>

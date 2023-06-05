4ec0d80b48bcc2b44be587987f583f930ef53e4c
leesemin <leesemin222@gmail.com>
2023-06-05 15:42:21 +0900

    로그인 페이지 마크업 중

--- develop/app.tsx
+++ develop/app.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
-import {createRoot} from "react-dom/client"
+import { createRoot } from "react-dom/client";
 
 function App() {
-    
-    return (
-      <div>
-        <h1>Title</h1>
-        <p>Hello World!</p>
-      </div>
-    )
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>Hello World!</p>
+    </div>
+  );
 }
 
 export default App;

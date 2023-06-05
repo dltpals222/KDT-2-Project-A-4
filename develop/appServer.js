6d984246c6f78b3d23ac0b68426ecc7eb15237c4
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-05 11:06:35 +0900

    express 앱 서버 구현 및 주석 첨부(app.use, static() 관련)

---
+++ develop/appServer.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+const app = express();
+
+// appServer 사용 가이드
+// 정적 파일 경로 설정하는 구문.
+// app.use의 경우 지정된 요청 주소명이 요청으로 들어왔을 때 콜백이 실행된다.
+// app.use("요청 주소명", 콜백함수),
+// 여기에 정적 파일을 응답할 수 있게 설정가능한데 방식은 다음과 같다.
+// app.use(express.static("경로명"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.get("/main.js", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "main.js"));
+});
+
+app.listen(3000, () => {
+    console.log("앱 서버가 http://localhost:3000 에서 실행 중입니다.");
+});

dc45a73ef0af028d0cd45873b0f0195c955dccdf
leesemin <leesemin222@gmail.com>
2023-06-05 15:42:21 +0900

    로그인 페이지 마크업 중

--- develop/app.tsx
+++ develop/app.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
-import {createRoot} from "react-dom/client"
+import { createRoot } from "react-dom/client";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-    
-    return (
-      <div>
-        <h1>Title</h1>
-        <p>Hello World!</p>
-      </div>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  );
 }
 
 createRoot(document.getElementById("root")!).render(<App />);

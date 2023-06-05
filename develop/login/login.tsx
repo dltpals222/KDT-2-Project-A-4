13ac4363b382fc602c547ab63bb0f217a67a5903
leesemin <leesemin222@gmail.com>
2023-06-05 16:48:24 +0900

    style 부분 추가

--- develop/login/login.tsx
+++ develop/login/login.tsx
@@ -19,13 +19,13 @@ function login() {
           <div>주식 추천 앱</div>
           <div>운운운운운운</div>
         </div>
-        <form action="GET">
-          <input type="text" placeholder="아이디"></input>
-          <input type="text" placeholder="패스워드"></input>
-          <input type="submit" value="로그인"></input>
+        <form action="/" method="GET">
+          <input style={inputTextStyle} type="text" placeholder="아이디"></input>
+          <input style={inputTextStyle} type="text" placeholder="패스워드"></input>
+          <input style={inputSubmitStyle} type="submit" value="로그인"></input>
         </form>
-        <form action="">
-          <input type="submit" value="회원가입" />
+        <form action="/signUp" method="GET">
+          <input style={inputSubmitStyle} type="submit" value="회원가입" />
         </form>
       </main>
     </div>

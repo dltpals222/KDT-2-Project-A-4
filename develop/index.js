6988ad8c9481b50ba89607a70a2fd8dd7b9e589a
dgchoi3904 <dgchoi3904@gmail.com>
2023-06-06 19:16:40 +0900

    webpack 기본 설정 및 테스트 완료

--- develop/index.js
+++
@@ -1,8 +0,0 @@
-function test() {
-    var str = "Hello World!, you call this at ";
-    var date = new Date();
-    str += date.toString();
-    return str;
-}
-console.log("이것은 엔트리 포인트입니다.");
-console.log(test());

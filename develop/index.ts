bdda3730cc1dab211b66a1b3a99a2ebe10a63cbc
dgchoi3904 <dgchoi3904@gmail.com>
2023-06-06 18:52:20 +0900

    테스트 확인, 에러 발생. 자세한 내용은 issue에 기입.

--- develop/index.ts
+++ develop/index.ts
@@ -1,1 +1,8 @@
+function test() : string {
+    let str : string = "Hello World!, you call this at "
+    const date : Date = new Date();
+    str += date.toString();
+    return str;
+}
 console.log("이것은 엔트리 포인트입니다.")
+console.log(test());

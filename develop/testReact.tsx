ed6f4dff645a6b6e112d4efd55db1afa289a8d13
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-07 15:15:07 +0900

    타입명 재정의, ReactNode가 아닌 JSX.Element로 변경

--- develop/testReact.tsx
+++ develop/testReact.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import {createRoot} from "react-dom/client";
 
-type ReactNode = React.ReactNode;
+// TS에서는 React.ReactNode를 배정하면 이해하지 못한다. 그렇기에 공식 홈페이지에서 이를 해석하기 위해 JSX 타입을 사용한다.
+// JSX 타입의 경우 함수 형식(JSX.Element)과 클래스 형식(JSX.ElementClass)으로 나뉜다. 
+type FCReactNode = JSX.Element;
 
-function App() : ReactNode {
+// 함수형 컴포넌트
+function App() : FCReactNode {
     return (
         <React.StrictMode>  
         </React.StrictMode>

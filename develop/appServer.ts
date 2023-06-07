063c73fa48ab8d90709c68195f0955a0738671db
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-07 16:22:17 +0900

    express로 appServer 기본 구문 작성.

---
+++ develop/appServer.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import path from "path";
+const app = express();
+
+// 사용할 정적 파일 폴더 경로 설정. 현재 develop으로 설정되어 있는것으로 보임.
+app.use(
+  express.static(path.join(__dirname, "./"), {
+    // 각 파일 타입별 헤더 설정. js시 헤더 컨텐츠 타입을 JS로.
+    setHeaders(res: Response, filePath: string) {
+      if (filePath.endsWith(".js")) {
+        res.setHeader("Content-Type", "application/javascript");
+      }
+    },
+  })
+);
+// 기본 도메인주소 요청 수신시 시작 Html 파일 전송.(HTML안에 번들 파일 연결 예정)
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+
+// 서버 실행
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});

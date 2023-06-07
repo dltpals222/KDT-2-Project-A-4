4b0cffcc6d3a70a79836a4e601029c5d9cb1ccf4
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-07 15:30:40 +0900

    라우터DOM이 적용되는지 테스트 구문 작성.

--- develop/testReact.tsx
+++ develop/testReact.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import {createRoot} from "react-dom/client";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 // TS에서는 React.ReactNode를 배정하면 이해하지 못한다. 그렇기에 공식 홈페이지에서 이를 해석하기 위해 JSX 타입을 사용한다.
 // JSX 타입의 경우 함수 형식(JSX.Element)과 클래스 형식(JSX.ElementClass)으로 나뉜다. 
 type FCReactNode = JSX.Element;
@@ -8,7 +8,12 @@ type FCReactNode = JSX.Element;
 // 함수형 컴포넌트
 function App() : FCReactNode {
     return (
-        <React.StrictMode>  
+        <React.StrictMode>
+            <BrowserRouter>
+            <Routes>
+                <Route path="/" element="test" />
+            </Routes>
+            </BrowserRouter>  
         </React.StrictMode>
     )
 }

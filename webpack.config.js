dbb7f0c1a4107253190cf87749a4aecd2a1f4577
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-07 16:04:42 +0900

    번들링 타겟을 Node.js로 설정후, CJS방식인 express의 의존성을 예외처리 지정

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
 const path = require("path");
 
 const config = {
-    entry: path.resolve(__dirname,"develop","testReact.tsx"),
+    entry: path.resolve(__dirname, "develop", "testReact.tsx"),
     mode: "development",
-    module : {
-        rules : [
+    module: {
+        rules: [
             //TS로더
             {
                 test: /\.tsx?$/,
                 exclude: /node_modules/,
                 use: "ts-loader",
             },
-            
+
         ],
     },
     resolve: {
-        extensions: [".tsx",".ts",".js"],
+        extensions: [".tsx", ".ts", ".js"],
     },
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname,"dist")
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    target: "node",
+    externals: {
+        express: "commonjs express",
     }
 };
 

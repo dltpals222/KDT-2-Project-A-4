ca2bca4fa606ec33b8cfed42465ab7a739bdc00c
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-07 15:33:16 +0900

    테스트용 연결할 하위 컴포넌트 작성. (Test.tsx)

---
+++ develop/Test.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+function Test() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <p>테스트용 페이지입니다. 아래 숫자가 보이시나요?</p>
+            <p>2023-06-07</p>
+        </div>
+    )
+}
+export default Test;

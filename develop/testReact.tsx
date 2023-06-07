82d97bf0d066659b8d3abc977cc85e9ec91a0866
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-07 15:34:29 +0900

    라우터 컴포넌트에 Test 컴포넌트 Import.

--- develop/testReact.tsx
+++ develop/testReact.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import {createRoot} from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// 하위 컴포넌트들.
+import Test from "./Test";
 // TS에서는 React.ReactNode를 배정하면 이해하지 못한다. 그렇기에 공식 홈페이지에서 이를 해석하기 위해 JSX 타입을 사용한다.
 // JSX 타입의 경우 함수 형식(JSX.Element)과 클래스 형식(JSX.ElementClass)으로 나뉜다. 
 type FCReactNode = JSX.Element;
@@ -11,7 +13,7 @@ function App() : FCReactNode {
         <React.StrictMode>
             <BrowserRouter>
             <Routes>
-                <Route path="/" element="test" />
+                <Route path="/" element="Test" />
             </Routes>
             </BrowserRouter>  
         </React.StrictMode>

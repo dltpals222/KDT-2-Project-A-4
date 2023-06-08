1b0c3cab6b58fbdf394f04565ba215b296e14c0d
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-08 16:11:06 +0900

    LoginPage에 inputId 구문 작성

--- develop/login/loginPage.tsx
+++ develop/login/loginPage.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LoginPage(){
-    
+    const [id, setId] = useState('');
+    const [pwd, setPwd] = useState('');
+
     return(
-        <div>
-            
-        </div>
+    <div>
+        <label htmlFor="id">
+            <input type="text" id="id" placeholder="ID를 입력하세요" name="id" value={} onChange={} />
+
+        </label>
+    </div>
     )
 }

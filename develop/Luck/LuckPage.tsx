0149058f949721089ebf81d0edbae9d58d84e772
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-06-08 18:06:16 +0900

    오늘의 운세 로직작성중

--- develop/Luck/LuckPage.tsx
+++ develop/Luck/LuckPage.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 
-const propsComponent = ({ luck, message }) => {
+interface PropsComponentType {
+  luck: string;
+  message: string;
+}
+
+const PropsComponent: React.FC<PropsComponentType> = ({ luck, message }) => {
   return (
     <div>
       <div>오늘의 운세: {luck}</div>
@@ -9,8 +14,8 @@ const propsComponent = ({ luck, message }) => {
   );
 };
 
-const luckMessage = () => {
-  const luckArr = [
+function LuckPage() {
+  const luckArr: string[] = [
     "대길",
     "중길",
     "길",
@@ -21,24 +26,35 @@ const luckMessage = () => {
     "중흉",
     "대흉",
   ];
-  const luckmessage = [
+  const luckmessageArr: string[] = [
     "오늘의 당신의 운은 대길 입니다. 뭘해도 잘되는 날이 될 것입니다. ",
     "오늘의 당신의 운은 중길 입니다. 당신은 길가다가도 돈을 줍게 되는 운입니다.",
     "오늘의 당신의 운은 길 입니다. 행운이 가득한 날이 될 것입니다.",
-    "오늘의 당신의 운은 소길 입니다. ",
+    "오늘의 당신의 운은 소길 입니다. 오늘은 너구리라면을 먹다가 다시마 2개가 있는 정도의 행운입니다.",
     "오늘의 당신의 운은 평 입니다. 평소와 똑같은 날이 될거 같습니다.",
-    "오늘의 당신의 운은 소흉 입니다. 당신은 걷다가 껌 밟을 운세입니다.",
-    "오늘의 당신의 운은 흉 입니다.",
-    "오늘의 당신의 운은 중흉 입니다.",
+    "오늘의 당신의 운은 소흉 입니다. 오늘 당신은 에러가 가득한 날이 될것입니다.",
+    "오늘의 당신의 운은 흉 입니다. 당신은 걷다가 새똥맞을 운세입니다.",
+    "오늘의 당신의 운은 중흉 입니다. 오늘은 당신의 차에 펑크가 나고 컴퓨터 그래픽카드가 고장날 운세이니 체념 하십시오.",
     "오늘의 당신의 운은 대흉 입니다. 길가다가도 머리가 빠질 운세이니 없는 듯이 사십시오.",
   ];
-};
 
-function luckPage(): JSX.Element {
+  const [luckIndex, setLuckIndex] = useState<number>(0);
+  const [luckMessageIndex, setLuckMessageIndex] = useState<number>(0);
+
+  const handleRandomLuck = () => {
+    const randomLuck: number = Math.floor(Math.random() * luckArr.length);
+    setLuckIndex(randomLuck);
+    setLuckMessageIndex(randomLuck);
+  };
+
   return (
     <div>
-      <propsComponent />
+      <PropsComponent
+        luck={luckArr[luckIndex]}
+        message={luckmessageArr[luckMessageIndex]}
+      />
+      <button onClick={handleRandomLuck}>운세 돌리기</button>
     </div>
   );
 }
-export default luckPage;
+export default LuckPage;
--- develop/Test.tsx
+++ develop/Test.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 
 function Test() : JSX.Element {
     return (
@@ -6,6 +7,7 @@ function Test() : JSX.Element {
             <h1>Hello World!</h1>
             <p>테스트용 페이지입니다. 아래 숫자가 보이시나요?</p>
             <p>2023-06-07</p>
+            <Link to="/luckpage">운세 테스트</Link>
         </div>
     )
 }

d0bf263be1837be9eb6f394724cb5be882c8996e
jin <ellenjin51@gmail.com>
2023-06-08 16:34:06 +0900

    메인페이지 내의 컴포넌트 구성

---
+++ develop/MainPageCom/StockChartBlock.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+function KOSbox () : JSX.Element{
+  return(
+    <>
+    <div>KOSPI차트</div>
+    <div>KOSDAQ</div>
+    <button>변환</button>
+    </>
+  );
+}
+
+export default KOSbox;
---
+++ develop/MainPageCom/StockReconmendBlock.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+function StockReco () : JSX.Element{
+  return(
+    <>
+    <div>거래량 기준</div>
+    <div>주식 가격 기준</div>
+    <button>변환</button>
+    </>
+  );
+}
+
+export default StockReco;

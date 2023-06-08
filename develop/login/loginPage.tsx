d6859615e5a5a414622e21fcd4860db39924d541
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-08 17:18:54 +0900

    에러 해결 시도 : 인터넷에서 나온 방법대로 타입을 명시

--- develop/login/loginPage.tsx
+++ develop/login/loginPage.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 
-function LoginPage(){
-    const [id, setId] = useState('');
-    const [pwd, setPwd] = useState('');
+function LoginPage() : JSX.Element{
+    const [id, setId] = useState<string>('');
+    const [pwd, setPwd] = useState<string>('');
 
     return(
     <div>
         <div id="logo">운에 맡겨라! 모의투자</div>
         <form>
             <label htmlFor="id">
-                <input type="text" id="id" placeholder="ID를 입력하세요" name="id" value={id} onChange={(e) => setId(e.target.value)} />
+                <input type="text" id="id" placeholder="ID를 입력하세요" name="id" value={id} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setId(e.target.value)} />
             </label>
             <label htmlFor='password'>
-                <input type="password" id='password' placeholder='password를 입력하세요' name='password' value={pwd} onChange={(e) => setPwd(e.target.value)} />
+                <input type="password" id='password' placeholder='password를 입력하세요' name='password' value={pwd} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPwd(e.target.value)} />
             </label>
             <button type="submit">로그인</button>
         </form>

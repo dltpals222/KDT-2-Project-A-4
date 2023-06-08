018b34aa16187cd45141197e7d3cc65879d5bbc7
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-08 16:13:58 +0900

    패스워드 입력부분 구문 추가, useState 추가.

--- develop/login/loginPage.tsx
+++ develop/login/loginPage.tsx
@@ -8,7 +8,9 @@ function LoginPage(){
     <div>
         <label htmlFor="id">
             <input type="text" id="id" placeholder="ID를 입력하세요" name="id" value={} onChange={} />
-
+        </label>
+        <label htmlFor='password'>
+            <input type="password" id='password' placeholder='password를 입력하세요' name='password' value={} onChange={} />
         </label>
     </div>
     )

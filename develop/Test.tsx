3daeb94c8fc5fd345b8f10c1979109e96613e4a2
jin <ellenjin51@gmail.com>
2023-06-08 16:57:54 +0900

    <hr>태그 추가

--- develop/Test.tsx
+++ develop/Test.tsx
@@ -12,7 +12,7 @@ function Test() : JSX.Element {
         <p>테스트용 페이지입니다. 아래 숫자가 보이시나요?</p>
         <p>2023-06-07</p>
         </div>
-
+        <hr />
         <div>
             <Navi />
             <KOSbox />

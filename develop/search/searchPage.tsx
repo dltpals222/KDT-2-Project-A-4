7e4e963543c2f9425c24bc0a4eed8070517c9099
leesemin <leesemin222@gmail.com>
2023-06-08 17:42:56 +0900

    검색페이지 만드는 중

---
+++ develop/search/searchPage.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
+
+interface SearchResult {
+  name: string;
+}
+
+// function performSearch(searchTerm: string, searchCategory: string): SearchResult[] {
+//   return [searchTerm, searchCategory];
+// }
+
+const SearchPage = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [searchCategory, setSearchCategory] = useState<string>("name");
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleCategoryChange = (event: ChangeEvent<HTMLSelectElement>): void => {
+    setSearchCategory(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    // const results = performSearch(searchTerm, searchCategory);
+    // setSearchResults(results);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <select onChange={handleCategoryChange} value={searchCategory}>
+          <option value="name">회사명</option>
+          <option value="code">회사코드</option>
+        </select>
+        <input type="text" value={searchTerm} onChange={handleInputChange} />
+        <input type="submit" value="확인" />
+      </form>
+      <ul></ul>
+    </div>
+  );
+};
+
+export default SearchPage;

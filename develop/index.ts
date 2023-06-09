a8ba7260fe83331daa1247b2b821a8f8cec4e514
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-09 16:30:04 +0900

    사용하지 않는 테스트 파일들 삭제

--- develop/index.ts
+++
@@ -1,8 +0,0 @@
-function test() : string {
-    let str : string = "Hello World!, you call this at "
-    const date : Date = new Date();
-    str += date.toString();
-    return str;
-}
-console.log("이것은 엔트리 포인트입니다.")
-console.log(test());

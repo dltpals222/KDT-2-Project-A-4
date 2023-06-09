e99a155ea65ff61cdccf0122b0e552b09715a51d
leesemin <leesemin222@gmail.com>
2023-06-09 17:05:15 +0900

    타입스크립트로 클래스 생성

--- develop/recommend/volume.tsx
+++ develop/recommend/volume.tsx
@@ -20,32 +20,3 @@ type CompanySamsungType = {
   day : string,
 }
 
-class companyDayVolume {
-  private company : {samsung : CompanySamsungType[]};
-  constructor(company : {samsung : CompanySamsungType[]}){
-    this.company = company
-  }
-
-  get dayVolume() : CompanyDateType {
-    return {
-      day : this.company.samsung.map((value) => {
-        const date = new Date(value.day);
-        const year = date.getFullYear();
-        const month = String(date.getMonth()+1).padStart(2,"0");
-        const day = String(date.getDate()).padStart(2,"0");
-        return `${year}-${month}-${day}`
-      }),
-      volume : this.company.samsung.map((value)=>value.volume)
-    }
-  }
-} 
-
-const samsungDate = new companyDayVolume(samsung).dayVolume
-const hanhwaDate = new companyDayVolume(hanhwa).dayVolume
-const hyundaiDate = new companyDayVolume(hyundai).dayVolume
-const NCDate = new companyDayVolume(NC).dayVolume
-const LGDate = new companyDayVolume(LG).dayVolume
-
-
-// console.log(new Date(samsung.samsung[0].day))
-console.log(samsungDate.day[0])

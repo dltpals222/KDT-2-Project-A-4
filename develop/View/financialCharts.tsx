c80bdb5bc7ed0d7c33891a2f112d5b602fd15c90
leesemin <leesemin222@gmail.com>
2023-06-09 15:34:41 +0900

    차트 생성 되는 것 확인

--- develop/View/financialCharts.tsx
+++ develop/View/financialCharts.tsx
@@ -1,8 +1,29 @@
 import * as React from 'react'
 import { ChartCanvas, Chart } from 'react-financial-charts';
 
-const MyChartComponent = () => {
-  // 차트 구현 및 설정
+interface DataPoint {
+  date : Date;
+  price : number;
+}
+
+const data : DataPoint[] =[
+  {date : new Date('2021-01-01'), price : 100},
+  {date : new Date('2021-01-02'), price : 150},
+  {date : new Date('2021-01-03'), price : 120},
+  {date : new Date('2021-01-04'), price : 190},
+  {date : new Date('2021-01-05'), price : 180},
+  {date : new Date('2021-01-06'), price : 170},
+  {date : new Date('2021-01-07'), price : 200},
+  {date : new Date('2021-01-08'), price : 0},
+  {date : new Date('2021-01-09'), price : -170},
+  {date : new Date('2021-01-10'), price : 50},
+  {date : new Date('2021-01-11'), price : 100},
+  {date : new Date('2021-01-12'), price : 500},
+  {date : new Date('2021-01-13'), price : 500},
+  {date : new Date('2021-01-14'), price : 500},
+]
+
+const FinancialChart : React.FC = () => {
   return (
     <ChartCanvas /* 차트 설정 */>
       <Chart /* 차트 유형 및 데이터 설정 */ />

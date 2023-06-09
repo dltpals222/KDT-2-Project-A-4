29195e190fd56e44ad5be6a579164b910019363f
leesemin <leesemin222@gmail.com>
2023-06-09 17:59:49 +0900

    회사 date의 day를 1999-01-01 형식으로 리턴하게 바꿈

--- develop/recommend/volume.tsx
+++ develop/recommend/volume.tsx
@@ -28,7 +28,13 @@ class companyDayVolume {
 
   get dayVolume() : CompanyDateType {
     return {
-      day : this.company.samsung.map((value) => new Date(value.day)),
+      day : this.company.samsung.map((value) => {
+        const date = new Date(value.day);
+        const year = date.getFullYear();
+        const month = String(date.getMonth()+1).padStart(2,"0");
+        const day = String(date.getDate()).padStart(2,"0");
+        return `${year}-${month}-${day}`
+      }),
       volume : this.company.samsung.map((value)=>value.volume)
     }
   }
@@ -42,4 +48,4 @@ const LGDate = new companyDayVolume(LG).dayVolume
 
 
 // console.log(new Date(samsung.samsung[0].day))
-console.log(new Date(samsungDate.day[0]).setFullYear)
+console.log(samsungDate.day[0])

22ed847124a0689eb186e5c56f1193afd563b850
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-06-09 11:22:52 +0900

    메인에서 마이페이지, 검색페이지, 운테스트 페이지 링크 걸기

--- develop/MainPageCom/NavigatorBar.tsx
+++ develop/MainPageCom/NavigatorBar.tsx
@@ -1,14 +1,20 @@
 import * as React from "react";
-
+import { Link } from "react-router-dom"
 
 
 
 function Navi() : JSX.Element {
   return  (
          <nav>
-            <button><a href=''>마이페이지</a></button>
-            <button><a href=''>검색페이지</a></button>
-            <button><a href=''>운 페이지</a></button>
+            <Link to="/mypage">
+            <button>마이페이지</button>
+            </Link>
+            <Link to="/search">
+            <button>검색페이지</button>
+            </Link>
+            <Link to="/luckpage">
+            <button>운 페이지</button>
+            </Link>
         </nav>
   );
 }
--- develop/testReact.tsx
+++ develop/testReact.tsx
@@ -2,9 +2,10 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // 하위 컴포넌트들.
-import Test from "./Test";
+import Main from "./Test";
 import LoginPage from "./login/loginPage";
 import LuckPage from "./Luck/LuckPage";
+import SearchPage from "./search/searchPage";
 // TS에서는 React.ReactNode를 배정하면 이해하지 못한다. 그렇기에 공식 홈페이지에서 이를 해석하기 위해 JSX 타입을 사용한다.
 // JSX 타입의 경우 함수 형식(JSX.Element)과 클래스 형식(JSX.ElementClass)으로 나뉜다.
 type FCReactNode = JSX.Element;
@@ -16,8 +17,10 @@ function App(): FCReactNode {
       <BrowserRouter>
         <Routes>
           {/* <Route path="/" element={<Test />} /> */}
-          <Route path="/" element={<LoginPage />} />
+          {/* <Route path="/" element={<LoginPage />} /> */}
+          <Route path="/" element={<Main />} />
           <Route path="/luckpage" element={<LuckPage />} />
+          <Route path="/search" element={<SearchPage />} />
         </Routes>
       </BrowserRouter>
     </React.StrictMode>

c1c20396bbdda0248756626df014cea3d8ed6408
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-09 12:53:01 +0900

    회원가입페이지에서 로직 적용(암호 체크, 길이 확인)

--- develop/signup/signUpPage.tsx
+++ develop/signup/signUpPage.tsx
@@ -17,32 +17,39 @@ function SignUpPage() {
   function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
     event.preventDefault();
     const data = { id, pwd, pwdConfirm };
-    fetch("/signup", {
-      // HTTP 요청 메서드 지정
-      method: "POST",
-      // HTTP 요청 헤더 지정
-      // 요청 본문에 클라이언트가 서버에 JSON 형태의 데이터를 담아 보낼 것을 명시
-      headers: { "Content-Type": "application/json" },
-      // HTTP 요청 본문에 담을 데이터를 지정
-      // id, pwd를 JSON 형태의 문자열로 변환하여 본문에 보냄
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        if (result.success) {
-          console.log("회원가입 성공", result);
-          alert("회원가입에 성공하셨습니다.");
-          navigate("/login");
-        } else {
-          // 로그인 실패시 사유를 alert으로 출력.(내용은 reason에 담겨짐.)
-          console.log("회원가입 실패");
-          alert(result.reason);
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        // 에러 처리 로직 추가
-      });
+    if(pwd !== pwdConfirm){
+        alert("비밀번호가 일치하지 않습니다.");
+    } else if(pwd.length < 6) {
+        alert("비밀번호는 최소 6자리 이상이어야 합니다.")
+    } else { 
+        fetch("/signup", {
+            // HTTP 요청 메서드 지정
+            method: "POST",
+            // HTTP 요청 헤더 지정
+            // 요청 본문에 클라이언트가 서버에 JSON 형태의 데이터를 담아 보낼 것을 명시
+            headers: { "Content-Type": "application/json" },
+            // HTTP 요청 본문에 담을 데이터를 지정
+            // id, pwd를 JSON 형태의 문자열로 변환하여 본문에 보냄
+            body: JSON.stringify(data),
+          })
+            .then((response) => response.json())
+            .then((result) => {
+              if (result.success) {
+                console.log("회원가입 성공", result);
+                alert("회원가입에 성공하셨습니다.");
+                navigate("/login");
+              } else {
+                // 로그인 실패시 사유를 alert으로 출력.(내용은 reason에 담겨짐.)
+                console.log("회원가입 실패");
+                alert(result.reason);
+              }
+            })
+            .catch((error) => {
+              console.error("Error:", error);
+              // 에러 처리 로직 추가
+            });
+    }
+    
   }
 
   return (

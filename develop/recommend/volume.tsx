9f9d80b84ddc1ca393d1bea38c0c6c25e7543fc7
leesemin <leesemin222@gmail.com>
2023-06-12 12:00:25 +0900

    리베이스 후 return의 day값이 변경된 것을 수정

--- develop/recommend/volume.tsx
+++ develop/recommend/volume.tsx
@@ -5,20 +5,12 @@ import hyundai from '../sourceData/financeDB(hyundai).json'
 import NC from '../sourceData/financeDB(NC).json'
 import * as React from 'react'
 
-/**
- * 회사 데이터타입
- * day : Date, string 배열 날짜
- * volume : number 배열 거래량
- */
 type CompanyDateType =  {
-  day : string[],
+  day : (Date | string)[],
   volume : number[],
 }
 
-/**
- * 주식 거래 데이터 타입
- */
-type CompanyInfoType = {
+type CompanySamsungType = {
   no : number,
   open : number,
   high : number,
@@ -28,30 +20,32 @@ type CompanyInfoType = {
   day : string,
 }
 
-/**
- * 주식 데이터를 받아올 때 날짜와 거래량을 따로 배열로 저장
- * 날짜는 string[], 거래량은 number[]로 받아옴
- */
-class CompanyDayVolumeInfo {
-  private company : {samsung : CompanyInfoType[]};
-  constructor(company : {samsung : CompanyInfoType[]}){
+class companyDayVolume {
+  private company : {samsung : CompanySamsungType[]};
+  constructor(company : {samsung : CompanySamsungType[]}){
     this.company = company
   }
 
-  /**
-   * 데이터(날짜, 거래량)를 가져옴
-   */
   get dayVolume() : CompanyDateType {
     return {
-      day : this.company.samsung.map((value) => new Date(value.day)),
+      day : this.company.samsung.map((value) => {
+        const date = new Date(value.day);
+        const year = date.getFullYear();
+        const month = String(date.getMonth()+1).padStart(2,"0");
+        const day = String(date.getDate()).padStart(2,"0");
+        return `${year}-${month}-${day}`
+      }),
       volume : this.company.samsung.map((value)=>value.volume)
     }
   }
 } 
 
-//거래량 기준 2일전의 종가와 1일전의 종가를 뺀 양 중 큰 거래량 TOP 5를 추천
-const samsungDate = new CompanyDayVolumeInfo(samsung).dayVolume
-const hanhwaDate = new CompanyDayVolumeInfo(hanhwa).dayVolume
-const hyundaiDate = new CompanyDayVolumeInfo(hyundai).dayVolume
-const NCDate = new CompanyDayVolumeInfo(NC).dayVolume
-const LGDate = new CompanyDayVolumeInfo(LG).dayVolume
+const samsungDate = new companyDayVolume(samsung).dayVolume
+const hanhwaDate = new companyDayVolume(hanhwa).dayVolume
+const hyundaiDate = new companyDayVolume(hyundai).dayVolume
+const NCDate = new companyDayVolume(NC).dayVolume
+const LGDate = new companyDayVolume(LG).dayVolume
+
+
+// console.log(new Date(samsung.samsung[0].day))
+console.log(samsungDate.day[0])

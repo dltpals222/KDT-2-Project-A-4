3df496aebbdfd4d34146d94661ee6250f205ed85
DGChoi3904 <dgchoi3904@gmail.com>
2023-06-13 15:31:08 +0900

    레거시 코드 삭제, data 상수명 loginInputData로 변경, 더미 주석 제거

--- develop/ViewModel/loginPage.tsx
+++ develop/ViewModel/loginPage.tsx
@@ -13,24 +13,19 @@ function LoginPage(): JSX.Element {
     alignItems: "center",
     height: "80%",
   };
+  
   // 목표 : 코드내 레거시 코드, 주석, 변수명 수정하기.
   function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
     // 기본 제출 이벤트 방지
     e.preventDefault();
-    const data = { id, pwd };
-    console.log(data);
-    // const form = event.target;
-    // const formData = new FormData(form);
-    // console.log(formData);
+    const loginInputData : Object = { id, pwd };
     fetch("/login", {
       // HTTP 요청 메서드 지정
       method: "POST",
       // HTTP 요청 헤더 지정
-      // 요청 본문에 클라이언트가 서버에 JSON 형태의 데이터를 담아 보낼 것을 명시
       headers: { "Content-Type": "application/json" },
       // HTTP 요청 본문에 담을 데이터를 지정
-      // id, pwd를 JSON 형태의 문자열로 변환하여 본문에 보냄
-      body: JSON.stringify(data),
+      body: JSON.stringify(loginInputData),
     })
       .then((response) => response.json())
       .then((result) => {
@@ -39,14 +34,13 @@ function LoginPage(): JSX.Element {
           alert("로그인 성공하셨습니다.");
           navigate("/main");
         } else {
-          // 로그인 실패시 사유를 alert으로 출력.(내용은 reason에 담겨짐.)
+          // 로그인 실패시 사유를 alert으로 출력.(내용은 reason에 담겨서 옴.)
           console.log("로그인 실패");
           alert(result.reason);
         }
       })
       .catch((error) => {
         console.error("Error:", error);
-        // 에러 처리 로직 추가
       });
   }
 

1d9d4aeaced038f753d88b0f607ae627d19ca903
leesemin <leesemin222@gmail.com>
2023-06-13 15:09:26 +0900

    dist/server_bundle.js 로 서버 실행시키기 위한 appServer.js의 파일 위치 변경

--- develop/Controller/appServer.ts
+++ develop/Controller/appServer.ts
@@ -9,7 +9,7 @@ const app = express();
 
 // 사용할 정적 파일 폴더 경로 설정. 현재 develop으로 설정되어 있는것으로 보임.
 app.use(
-  express.static(path.join(__dirname, "..", "..", "dist"), {
+  express.static(path.join(__dirname), {
     // 각 파일 타입별 헤더 설정. js시 헤더 컨텐츠 타입을 JS로.
     setHeaders(res: Response, filePath: string) {
       if (filePath.endsWith(".js")) {
@@ -23,7 +23,7 @@ app.use(express.json());
 
 // 기본 도메인주소 요청 수신시 시작 Html 파일 전송.(HTML안에 번들 파일 연결 예정)
 app.get("/", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "..", "..", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.post("/signup", async (req: Request, res: Response) => {

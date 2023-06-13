e4ca7b367a58306f13330793d575b1e506c3a9b7
leesemin <leesemin222@gmail.com>
2023-06-13 15:33:00 +0900

    검색페이지 자료 정리 함수 만드는 중

--- develop/View/financialCharts.tsx
+++ develop/View/financialCharts.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react'
-import { ChartCanvas, Chart } from 'react-financial-charts';
+// import * as React from 'react'
+// import { ChartCanvas, Chart } from 'react-financial-charts';
 
-const MyChartComponent = () => {
-  // 차트 구현 및 설정
-  return (
-    <ChartCanvas /* 차트 설정 */>
-      <Chart /* 차트 유형 및 데이터 설정 */ />
-    </ChartCanvas>
-  );
-};
+// const MyChartComponent = () => {
+//   // 차트 구현 및 설정
+//   return (
+//     <ChartCanvas /* 차트 설정 */>
+//       <Chart /* 차트 유형 및 데이터 설정 */ />
+//     </ChartCanvas>
+//   );
+// };

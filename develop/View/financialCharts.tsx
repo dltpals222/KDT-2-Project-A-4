91e3ef19b7d20ad65e29f289013690a90c632c18
leesemin <leesemin222@gmail.com>
2023-06-16 17:29:56 +0900

    라우터 폴더 생성 후 파일 생성

--- develop/View/financialCharts.tsx
+++ develop/View/financialCharts.tsx
@@ -4,28 +4,6 @@ import { ChartCanvas, Chart } from "react-financial-charts";
 import { LineSeries } from "react-financial-charts/lib/index";
 import { XAxis, YAxis } from "react-financial-charts/lib/index";
 
-interface DataPoint {
-  date : Date;
-  price : number;
-}
-
-const data : DataPoint[] =[
-  {date : new Date('2021-01-01'), price : 100},
-  {date : new Date('2021-01-02'), price : 150},
-  {date : new Date('2021-01-03'), price : 120},
-  {date : new Date('2021-01-04'), price : 190},
-  {date : new Date('2021-01-05'), price : 180},
-  {date : new Date('2021-01-06'), price : 170},
-  {date : new Date('2021-01-07'), price : 200},
-  {date : new Date('2021-01-08'), price : 0},
-  {date : new Date('2021-01-09'), price : -170},
-  {date : new Date('2021-01-10'), price : 50},
-  {date : new Date('2021-01-11'), price : 100},
-  {date : new Date('2021-01-12'), price : 500},
-  {date : new Date('2021-01-13'), price : 500},
-  {date : new Date('2021-01-14'), price : 500},
-]
-
 const FinancialChart : React.FC = () => {
   return (
     <ChartCanvas data={data} width={380} height={300} xAccessor={(d)=> d.date } xScale={scaleTime()} ratio={1} seriesName="Kospi">

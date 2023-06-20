7c279e7877455b9709b08cd44681aca6ad09bbf6
leesemin <leesemin222@gmail.com>
2023-06-20 16:16:32 +0900

    스타일 지정

--- develop/View/StockChartBlock.tsx
+++ develop/View/StockChartBlock.tsx
@@ -2,11 +2,11 @@ import * as React from "react";
 
 function KOSbox () : JSX.Element{
   return(
-    <>
+    <div style={{marginBottom : "2.5px"}}>
     <div>KOSPI차트</div>
     <div>KOSDAQ</div>
     <button>변환</button>
-    </>
+    </div>
   );
 }
 

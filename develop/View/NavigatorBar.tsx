c56bcf7e73171ce7392b3833a84d1b1fe2f53830
leesemin <leesemin222@gmail.com>
2023-06-20 16:08:39 +0900

    간단한 스타일 지정

--- develop/View/NavigatorBar.tsx
+++ develop/View/NavigatorBar.tsx
@@ -4,19 +4,28 @@ import { Link } from "react-router-dom"
 
 
 function Navi() : JSX.Element {
+  const navStyle = {
+    height : "48px", 
+    width: "390px",
+    marginBottom : "5px",
+    display : "flex",
+    justifyContent : "space-evenly",
+    alignItems : "center"
+  }
+
   return  (
-         <nav>
-            <Link to="mypage">
-            <button type="button">마이페이지</button>
+         <nav style={navStyle}>
+            <Link to="main">
+            <button type="button">메인페이지</button>
             </Link>
             <Link to="search">
             <button type="button">검색페이지</button>
             </Link>
-            <Link to="luckpage">
-            <button type="button">운 페이지</button>
-            </Link>
             <Link to="companypage">
             <button type="button">회사 상세 페이지</button>
+            </Link>
+            <Link to="luckpage">
+            <button type="button">운 페이지</button>
             </Link>
         </nav>
   );
